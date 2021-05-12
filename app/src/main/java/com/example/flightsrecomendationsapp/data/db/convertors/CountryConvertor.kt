f9544be89156ecc30019ce7d68351033126250f7
MarkoMiljkovic <studio.vikings.apps.@gmail.com>
2021-05-13 01:46:59 +0200

    Added type convertor in DB

---
+++ app/src/main/java/com/example/flightsrecomendationsapp/data/db/convertors/CountryConvertor.kt
@@ -0,0 +1,20 @@
+package com.example.flightsrecomendationsapp.data.db.convertors
+
+import androidx.room.TypeConverter
+import com.example.flightsrecomendationsapp.data.db.entities.CountryEntity
+
+class CountryConvertor {
+    @TypeConverter
+    fun fromString(value: String?): CountryEntity? {
+        val splited = value?.split(":")
+        return if (!splited.isNullOrEmpty() && splited.size == 2)
+            CountryEntity(splited[0], splited[1])
+        else
+            null
+    }
+
+    @TypeConverter
+    fun fromCountryEntity(country: CountryEntity?): String {
+        return "${country?.code}:${country?.name}"
+    }
+}
