382492dcbfb88e1f87250d846e16695a10f41945
MarkoMiljkovic <studio.vikings.apps.@gmail.com>
2021-05-13 01:46:09 +0200

    Added parent classes for DTO and DAO classes, for better and easier conversion between them

---
+++ app/src/main/java/com/example/flightsrecomendationsapp/data/publicmodel/CountryData.kt
@@ -0,0 +1,8 @@
+package com.example.flightsrecomendationsapp.data.publicmodel
+
+open class CountryData {
+    @Transient
+    open val code: String = ""
+    @Transient
+    open val name: String = ""
+}
