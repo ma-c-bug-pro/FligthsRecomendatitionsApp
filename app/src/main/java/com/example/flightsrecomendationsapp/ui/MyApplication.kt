5a516be864ec56ecac860871d669a186fc324661
MarkoMiljkovic <studio.vikings.apps.@gmail.com>
2021-05-15 19:15:30 +0200

    Moved application class from util

--- app/src/main/java/com/example/flightsrecomendationsapp/ui/MyApplication.kt
+++ app/src/main/java/com/example/flightsrecomendationsapp/ui/MyApplication.kt
@@ -1,6 +1,9 @@
-package com.example.flightsrecomendationsapp.util
+package com.example.flightsrecomendationsapp.ui
 
 import android.app.Application
+import com.example.flightsrecomendationsapp.util.DbModule
+import com.example.flightsrecomendationsapp.util.NetworkModule
+import com.example.flightsrecomendationsapp.util.RepoModule
 import com.example.flightsrecomendationsapp.util.di.AppModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
