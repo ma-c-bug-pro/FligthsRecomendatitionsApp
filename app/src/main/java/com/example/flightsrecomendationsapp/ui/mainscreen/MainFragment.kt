8c1ba9c60dccf1eea363c9dcd78cd47eb89f120e
MarkoMiljkovic <studio.vikings.apps.@gmail.com>
2021-05-15 19:00:02 +0200

    Removed unnecessary files Finished main screen design and functionality

--- app/src/main/java/com/example/flightsrecomendationsapp/ui/mainscreen/MainFragment.kt
+++ app/src/main/java/com/example/flightsrecomendationsapp/ui/mainscreen/MainFragment.kt
@@ -1,26 +1,36 @@
 package com.example.flightsrecomendationsapp.ui.mainscreen
 
+import android.Manifest
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.flightsrecomendationsapp.data.repositories.FlightRepo
+import androidx.navigation.fragment.findNavController
 import com.example.flightsrecomendationsapp.databinding.FragmentMainBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import com.example.flightsrecomendationsapp.util.LocationUtil
+import com.example.flightsrecomendationsapp.util.LocationUtil.Companion.roundToXDecimalPlaces
+import com.example.flightsrecomendationsapp.util.PermissionFragmentDialog
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.koin.android.ext.android.inject
 
 
 class MainFragment : Fragment() {
 
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
     private lateinit var binding: FragmentMainBinding
 
-    private val repo: FlightRepo by inject()
+    private val locationUtil: LocationUtil by inject()
+
+    private val appPrefs: SharedPreferences by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        locationUtil.registerForActivityResult(this@MainFragment)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
     }
 
     override fun onCreateView(
@@ -33,13 +43,55 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        GlobalScope.launch {
-            val call =
-                repo.getBestFlights("LON", "anywhere", "11/05/2021", "14/05/2021", "10/05/2021")
-            call.collect {
-                println("Result: $it")
-                println("Response count: ${it.data?.size}")
+        binding.getNearestBtn.setOnClickListener{
+            checkNeededPermissions()
+        }
+    }
+
+    private fun checkNeededPermissions() {
+        if (!locationUtil.checkPermissions(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) && appPrefs.getBoolean("ask_loc_permissions", true)
+        ) {
+            showPermissionDialog()
+        } else {
+            goToOffersFragment()
+        }
+    }
+
+    private fun goToOffersFragment() {
+
+        if (locationUtil.isLocationEnabled(requireActivity())) {
+            var location = ""
+            locationUtil.requestNewLocationData(mFusedLocationClient) { lat, lon ->
+                location = "${lat.roundToXDecimalPlaces(2)}-${lon.roundToXDecimalPlaces(2)}-250km"
+                val action = MainFragmentDirections.actionMainFragmentToPopularOfferFragment(location)
+                findNavController().navigate(action)
             }
+
+        } else {
+            locationUtil.enableLocationSource(requireActivity())
+        }
+    }
+
+    private fun showPermissionDialog() {
+        if (!locationUtil.checkPermissions(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        ) {
+            PermissionFragmentDialog(allowPermission = {
+                locationUtil.requestPermission(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) {
+                    goToOffersFragment()
+                }
+            },
+                declinePermission = {
+
+                },
+                declineForeverPermission = {
+                    appPrefs.edit().putBoolean("ask_loc_permissions", false).apply()
+                }).show(childFragmentManager, "permission_dialog_fragment")
         }
     }
 }
--- app/src/main/java/com/example/flightsrecomendationsapp/ui/mainscreen/MainViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flightsrecomendationsapp.ui.mainscreen
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel: ViewModel() {
-}
